69058f7821b30d05fd6d99629ccfc713b07d5030
Gabriel Gustavo Nicolodi <gabrielgusn@gmail.com>
2026-05-09 03:03:44 +0400

    feature: adding github and linkedin buttons, fixing ::before blur and improving responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log(
+  '%cI know what kind of man you are',
+  'background:#000;color:#55FF55;font-size:20px;font-weight:bold;padding:10px 24px;border:2px solid #55FF55;letter-spacing:2px;text-shadow:0 0 8px #55FF55'
+)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
